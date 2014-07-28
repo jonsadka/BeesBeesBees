cc9f9a960bf4a7b32eb8531bd6659a7525ff774c
Hack Reactor Students <Jonsadka@gmail.com>
2014-07-28 15:00:18 -0700

    Completed BeesBeesBees

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,16 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(Grub.prototype);
+RetiredForagerBee.prototype.forrage = function(){
+  return 'I am too old, let me play cards instead';
+};
+RetiredForagerBee.prototype.gamble = function(treasure){
+  this.treasureChest.push(treasure);
+};
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
